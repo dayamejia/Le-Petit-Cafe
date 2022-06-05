5cac4203b3778a8120ad4294b1d9c47fd6c87d53
admin <admin@DAYANA>
2022-06-05 17:02:26 -0600

    Agregamos una Lista de Productos

---
+++ LePetitCafe/LePetitCafe.Web/Controllers/ProductosController.cs
@@ -0,0 +1,35 @@
+﻿using LePetitCafe.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LePetitCafe.Web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+            var producto1 = new ProductoModel();
+            producto1.Id = 1;
+            producto1.Descripcion = "Cafe Frappe";
+
+            var producto2 = new ProductoModel();
+            producto2.Id = 2;
+            producto2.Descripcion = "Cafe Frio";
+
+            var producto3 = new ProductoModel();
+            producto3.Id = 3;
+            producto3.Descripcion = "Cappuccino Frappe";
+
+            var listadeProductos = new List<ProductoModel>();
+            listadeProductos.Add(producto1);
+            listadeProductos.Add(producto2);
+            listadeProductos.Add(producto3);
+
+            return View(listadeProductos);
+        }
+    }
+}
