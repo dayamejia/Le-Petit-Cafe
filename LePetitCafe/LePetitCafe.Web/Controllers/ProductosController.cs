7e71e59632a182168a490ff2a603788eae2567c8
admin <admin@DAYANA>
2022-06-05 19:56:17 -0600

    Agregando Arquitectura Basica

--- LePetitCafe/LePetitCafe.Web/Controllers/ProductosController.cs
+++ LePetitCafe/LePetitCafe.Web/Controllers/ProductosController.cs
@@ -1,4 +1,4 @@
-﻿using LePetitCafe.Web.Models;
+﻿using LePetitCafe.BL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,22 +12,8 @@ namespace LePetitCafe.Web.Controllers
         // GET: Productos
         public ActionResult Index()
         {
-            var producto1 = new ProductoModel();
-            producto1.Id = 1;
-            producto1.Descripcion = "Cafe Frappe";
-
-            var producto2 = new ProductoModel();
-            producto2.Id = 2;
-            producto2.Descripcion = "Cafe Frio";
-
-            var producto3 = new ProductoModel();
-            producto3.Id = 3;
-            producto3.Descripcion = "Cappuccino Frappe";
-
-            var listadeProductos = new List<ProductoModel>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProductos();
 
             return View(listadeProductos);
         }
---
+++ LePetitCafe/LePetitCafe.WebAdmin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LePetitCafe.WebAdmin.MvcApplication" Language="C#" %>
